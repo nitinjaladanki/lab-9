6422adb7ec99939709269a4481a2464c36d14945
Nitin Jaladanki <nitin@nitins-macbook.lan>
2021-07-12 11:57:01 -0700

    openning graph

--- image_processing.py
+++ image_processing.py
@@ -1,5 +1,28 @@
-# import modules needed
-import numpy as np # numerical functions
-import matplotlib.pyplot as plt # plotting tools
-import cv2 # opencv
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
 
+img = cv2.imread("baby_turtle.jpg") 
+dimensions = img.shape
+print(dimensions)
+blue_turtle = img[:,:,0]
+green_turtle = img[:,:,1]
+red_turtle = img[:,:,2]
+fig, ax = plt.subplots(1,3, figsize = (15, 10))
+ax[0].imshow(red_turtle, cmap="Reds")
+ax[1].imshow(green_turtle, cmap="Greens")
+ax[2].imshow(blue_turtle, cmap="Blues")
+plt.show()
+
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) 
+
+hue_turtle = hsv[:,:,0]
+sat_turtle = hsv[:,:,1]
+val_turtle = hsv[:,:,2]
+
+fig, ax = plt.subplots(1,3, figsize = (15, 10))
+ax[0].imshow(hue_turtle, cmap="hsv")
+ax[1].imshow(sat_turtle, cmap="binary")
+ax[2].imshow(val_turtle, cmap="Oranges")
+fig.colorbar(plt.cm.ScalarMappable(cmap="hsv"), ax=ax)
+plt.show()
